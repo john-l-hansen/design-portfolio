c112a43c000f45c3c930f65eed47624ad03dd2c7
john-l-hansen <john.l.hansen@gmail.com>
2020-09-26 22:25:19 -0700

    update logo and date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,8 +53,8 @@ const Footer = () => (
         <Link to="/">
             <Logo />
         </Link>
-        <FooterAuthor href="https://www.marguerite.io/?utm_source=prist&utm_medium=footer&utm_campaign=prist_starter">
-            © 2019 — Designed & developed by Marguerite Roth
+        <FooterAuthor href="https://www.marguerite.io/?utm_source=prist&utm_medium=footer&utm_campaign=prist_starterhttps://www.linkedin.com/in/john-hansen-51796829/">
+            © 2020 — John Hansen
             <FooterSpooch className="FooterSpooch" src={spooch} />
         </FooterAuthor>
     </FooterContainer>
